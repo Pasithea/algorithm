b335773cd467c09618a21b2365ad86e014206d12
Pasithea <Tsukaicyan@gmail.com>
2018-11-30 14:28:31 +0800

    Add bubble sort.

---
+++ py3k/bubble_sort.py
@@ -0,0 +1,24 @@
+"""
+    Bubble Sort.
+"""
+
+
+class BubbleSort:
+    def sort(self, l: list) -> list:
+        length = len(l)
+        if length <= 1: return l
+        for i in range(length):
+            flag = False
+            for j in range(length-1):
+                if l[j] > l[j+1]:
+                    l[j], l[j+1] = l[j+1], l[j]
+                    flag = True
+            if not flag:
+                break
+        return l
+
+
+if __name__ == '__main__':
+    bs = BubbleSort()
+    l = [4, 1, 6, 7, 9, 3]
+    print(bs.sort(l))
