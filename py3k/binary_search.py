121f613f6b7d357ff8f6d245cb717c6a6d163362
Pasithea <Tsukaicyan@gmail.com>
2019-01-06 16:09:23 +0800

    Improved binary search algorithm.

--- py3k/binary_search.py
+++ py3k/binary_search.py
@@ -10,17 +10,20 @@ class BinarySearch:
         low, high = 0, len(sorted_list) - 1
         while low <= high:
             mid = low + (high - low) // 2
-            if sorted_list[mid] == target:
-                return mid
-            elif sorted_list[mid] < target:
+            if sorted_list[mid] < target:
                 low = mid + 1
-            else:
+            elif sorted_list[mid] > target:
                 high = mid - 1
+            else:
+                if mid == 0 or sorted_list[mid-1] != target:
+                    return mid
+                else:
+                    high = mid - 1
         return None
 
 
 if __name__ == '__main__':
     bs = BinarySearch()
-    sorted_list = [1, 3, 5, 7, 9, 10, 20, 40, 60, 80, 100]
+    sorted_list = [1, 3, 5, 7, 10, 10, 10, 40, 60, 80, 100]
     for i in reversed(sorted_list):
         print(bs.bsearch(sorted_list, i))
