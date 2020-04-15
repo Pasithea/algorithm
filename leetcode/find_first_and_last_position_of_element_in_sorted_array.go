743c0480adcbed82be162c963fcbabf36be1cf08
Pasithea <Tsukaicyan@gmail.com>
2020-04-15 17:46:10 +0800

    New solutions.

---
+++ leetcode/find_first_and_last_position_of_element_in_sorted_array.go
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+
+func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+	start, end := 0, len(nums) - 1
+	i, j := -1, -1
+	for start <= end {
+		mid := (start + end) / 2
+		if nums[mid] > target {
+			end = mid - 1
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			if (mid - 1) >= start && nums[mid-1] == target {
+				end = mid - 1
+			} else {
+				i = mid
+				break
+			}
+		}
+	}
+	if i != -1 {
+		start, end = i, len(nums) - 1
+		for start <= end {
+			mid := (start + end) / 2
+			if nums[mid] > target {
+				end = mid - 1
+			} else if nums[mid] < target {
+				start = mid + 1
+			} else {
+				if (mid + 1) <= end && nums[mid+1] == target {
+					start = mid + 1
+				} else {
+					j = mid
+					break
+				}
+			}
+		}
+		if j == -1 {
+			j = 0
+		}
+	}
+	return []int{i, j}
+}
---
+++ leetcode/kth_largest_element_in_an_array.go
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+
+func findKthLargest(nums []int, k int) int {
+	heap := make([]int, 0, k)
+	for i := 0; i < len(nums); i++ {
+		if len(heap) < k {
+			heap = append(heap, nums[i])
+			up(heap, len(heap)-1)
+		} else {
+			if nums[i] > heap[0] {
+				heap[0] = nums[i]
+				down(heap, 0, len(heap)-1)
+			}
+		}
+	}
+	return heap[0]
+}
+
+func up(heap []int, n int) {
+	for n > 0 {
+		p := (n - 1) / 2
+		if n == p {
+			return
+		}
+		if heap[n] < heap[p] {
+			heap[p], heap[n] = heap[n], heap[p]
+		} else {
+			return
+		}
+		n = p
+	}
+}
+
+func down(heap []int, n, k int) {
+	for n <= k {
+		c := n
+		c1 := c * 2 + 1
+		c2 := c1 + 1
+		if c1 <= k && heap[c1] < heap[c] {
+			c = c1
+		}
+		if c2 <= k && heap[c2] < heap[c] {
+			c = c2
+		}
+		if c == n {
+			return
+		}
+		heap[c], heap[n] = heap[n], heap[c]
+		n = c
+	}
+}
