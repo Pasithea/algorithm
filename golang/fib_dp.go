372b3f2a0a616bd3835acca37f1e362dc77e1102
Pasithea <Tsukaicyan@gmail.com>
2020-04-18 20:07:23 +0800

    Simple dp.

---
+++ golang/fib_dp.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(fib(2))
+}
+
+func fib(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n, n)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
+}
